12a0a25e0a1bc3ae685e6fad76a72b67d2d66936
jmz456 <jamesmiller78@gmail.com>
2014-05-16 16:48:29 -0400

    updated solution

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,9 +4,15 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
+#define a method called total
+#calculate sum of the array
+
+
+#define a method called sentence_maker
+#xxconcatinatexx join the elements of the array so the output is one string
 
 # What is the input?
 # What is the output? (i.e. What should the code return?)
@@ -14,12 +20,53 @@
 
 
 # 2. Initial Solution
+=begin
+def total(array)
+	array = array.inject(0) {|sum,x| sum + x }
 
+end
 
-
+def sentence_maker(array)
+	array = array.join(" ").capitalize.push "."
+end
+=end
 
 # 3. Refactored Solution
+ 
+def total(array)
+	array.inject(0) {|sum,x| sum + x }
 
+end
 
+def sentence_maker(array)
+	array.join(" ").capitalize.<< "."
+end
+ 
 
 # 4. Reflection 
+=begin
+My initial strategy was a total failure.  Instead of creating one method to calculate a total
+of an array, I thought I was supposed to create two methods.	As my errors kept bouncing
+back and forth, I attempted to restart and just create one because if one array worked they maybe
+it would in both arrays.  This is where I discovered that it would be one method accepting an array.
+
+As I approached my strategy again, I fixed my pseudocode to reflect one method.  My next challenge
+was calculating the sum.  At first, I thought the task was easy.  Then I realized I don't think
+I have ever returned the sum of an array before.  The challenge was a google search for methods
+that I've never seen before.  I referenced the exercises prior to phase 0 and my notes from codecademy,
+but I couldn't find something this simple.  There was a way while setting the sum to equal zero prior 
+to the solution, but that proved an excessive challenge.  I referred to stackoverflow.com for some ideas 
+and found a great solution using the .inject method.  While I understand that this takes an argument
+and a block, I'm still not entirely sure how it works.  I played with the code for about 30 minutes.
+
+I had seen the .join method in the past and so applied it to the sentence_maker method.  However, I resulted
+in google searches looking for how to use this method correctly.  I think one of my biggest pitfalls
+was I used testing as a trial and error.  Perhaps I should be using the irb.  
+
+One of the other things that was helpful was displaying the method results while I was testing.  It helped
+me identify what was wrong with my coding and syntax.
+
+I went back through codecademy at the beginning of this week and ran through all the exercises again.  That 
+was probably very helpful because I felt rusty from not using ruby for some time and having such a hard time
+with JavaScript last week.
+=end
--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -7,19 +7,77 @@
 # I worked on this challenge [by myself, with: ].
 
 # 1. Pseudocode
-
+#define a method called get_grade
+#calculate the mean of an array which will output the sum divided total elements of the array
+#if the result of the array is greater than 90, return a letter grade of 'A'
+#if the result of the array is greater than 80, return a letter grade of 'B'
+#if the result of the array is greater than 70, return a letter grade of 'C'
+#if the result of the array is greater than 60, return a letter grade of 'D'
+#if the result of the array is less than 60, return a letter grade of 'F'
 # What is the input?
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
 
 # 2. Initial Solution
+=begin
+def get_grade(array, *y)
+	array = array.inject(0) {|x,y| (x + y)} / array.length
+	print array
+	if array >= 90
+		return "A"
+	elsif array >= 80
+		return "B"
+	elsif array >= 70
+		return "C"
+	elsif array >= 60
+		return "D"
+	elsif array < 60
+		return "F"
 
-
-
+end
+end
+=end
 
 # 3. Refactored Solution
 
+def get_grade(array, *y)
+	array = array.inject(0) {|x,y| (x + y)} / array.length
+	
+	if array >= 90   # don't understand why I can't refactor: "A" if array >= 90
+		 "A"
+	elsif array >= 80
+		 "B"
+	elsif array >= 70
+		 "C"
+	elsif array >= 60
+		 "D"
+	elsif array < 60
+		 "F"
 
+end
+end
 
 # 4. Reflection 
+=begin
+My initial strategy was perfect.  I understood how to calculate sum, so I would just have to 
+divide by the array length.  If the result of the array was greater than 90, or 80, I would 
+return the respective letter grade.  I spent 3 hours on this trying to figure out why it wouldn't work.
+I turns out that prior to completing this challenge, I wrote a little code and include 'arrayA' in
+one of my methods.  I must have been reading my errors wrong until I hit a lightbulb.  Problem solved,
+good code.  deleted the 'A' in 'arrayA' and that fixed everything.  I was still under the impression 
+that I had to write two methods.  Once again, after discovering from the first exercise that only 1 was
+needed, everything else made sense.  My code worked perfectly.
+
+I understand that ruby will implicitly return a statement without using "return." However I can seem to 
+refactor beyond this.  I thought I could refactor line 47 to a single line, but it doesn't seem to work.
+The wildcard '*' is something I barely remembered from codecademy.  A wildcard is good when the number of elements of an
+array to be entered are unknown.  I don't know how I remembered this, but my method doesn't work without it.
+
+Testing is fun as long as the results and code come-up within a short period of time.  The feeling of victory
+is probably the greatest part of the challenge.  The worst part is running test after test and not 
+understanding the errors from your TDD.
+
+I wouldn't say I'm comfortable with the learning competencies.  I don't think I can complete them without
+a bit of research or referring to my notes.
+=end
