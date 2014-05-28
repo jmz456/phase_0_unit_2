6d76475eff6412fa4d37eecbd0a3b57a7c43a87b
jmz456 <jamesmiller78@gmail.com>
2014-05-28 00:19:11 -0400

    reworked part 3

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -2,10 +2,10 @@
 
 # I worked on this challenge [by myself].
 
-boggle_board = [["b", "r", "a", "e"],
-                ["i", "o", "d", "t"],
-                ["e", "c", "l", "r"],
-                ["t", "a", "k", "e"]]
+boggle_board = [["b", "r", "a", "e"], 
+				["i", "o", "d", "t"], 
+				["e", "c", "l", "r"], 
+				["t", "a", "k", "e"]]
 
 
 # Part 1: Access multiple elements of a nested array
@@ -15,23 +15,23 @@ boggle_board = [["b", "r", "a", "e"],
 # 	a. define a method called create_word that takes the parameters board, and *coords
 # 	b. coords has an undefined number of parameters
 # 	c. map those to another array and run coord through each element 
-# 	d. set your bock of code to join your first parameter and last parameter.
+# 	d. set your block of code to join your first parameter and last parameter.
 
 
 
 # Initial Solution
-# def create_word(board, *coords)
-# 	coords.map {|coord| board[coord.first][coord.last]}.join("")
-# end
+def create_word(board, *coords)
+	coords.map {|coord| board[coord.first][coord.last]}.join("")      #.first returns the first index and .last returns the last index
+end
 
-# Refactored Solution
+Refactored Solution
 def create_word(board, *coords)
 	coords.map {|coord| board[coord.first][coord.last]}.join("")
 end
 
 # # DRIVER TESTS GO BELOW THIS LINE
 puts create_word(boggle_board, [2,1],[1,1],[1,2],[0,3]) # => returns "code" row 2 col 1 / row 1 col 1 / row 1 col 2/ row 0 col 3
-puts create_word(boggle_board, [0,1],[0,2],[1,2]) # => creates what california slang word
+puts create_word(boggle_board, [0,1],[0,2],[1,2]) # => creates what california slang word  RAD
 puts create_word(boggle_board, [0,0],[1,0],[0,1],[1,2]) # => bird
 # Reflection 
 # Had a curly brace in the middle of all that.  Drove me nuts.  I'm not sure how to refactor the CODE!  It's pretty RAD!
@@ -58,7 +58,10 @@ end
 # # DRIVER TESTS GO BELOW THIS LINE
 p get_row(1, boggle_board) #=>  ["i", "o", "d", "t"]
 
-
+# boggle_board = [["b", "r", "a", "e"], 
+# 				["i", "o", "d", "t"], 
+# 				["e", "c", "l", "r"], 
+# 				["t", "a", "k", "e"]]
 # Reflection 
 #How is this so hard?!  I can't event think straight!  I totally found this by luck.
 
@@ -71,17 +74,21 @@ p get_row(1, boggle_board) #=>  ["i", "o", "d", "t"]
 # Part 3: write a method that takes a column number and returns all the elements in the column.
 # we need to pull the index and then the subindex
 # Initial Solution
- def get_col(array_index, array_subindex)
-	#first array(index=0)  second subarry(index=1)
-
+ def get_col(col, boggle_board)
+	boggle_board.map {|x| x.fetch(col)} # so the .fetch method calls the index of each sub-array.
+	
  end
 
- get_col(1)  #=>  ["r", "o", "c", "a"]
+p get_col(0, boggle_board)  #=>  ["b", "i", "e", "t"]    asks for every index 1 sub-index in each index
+p get_col(1, boggle_board)  # => ["r", "o", "c", "a"]
+p get_col(2, boggle_board)  # => ["a", "d", "l", "k"]
+p get_col(3, boggle_board)  # => ["e", "t", "r", "e"] 
+
  # Refactored Solution
 #clueless
 
 # # DRIVER TESTS GO BELOW THIS LINE
-puts boggle_board[0][1] == "r"   # returns boggle_board[row_number][column_number]
+puts boggle_board[0][1] == "r"   # returns boggle_board[row_number][column_number]  true
 puts boggle_board[2][1] == "c" #=> should be true
 puts boggle_board[3][3] == "e" #=> should be true
 puts boggle_board[2][3] == "x" #=> should be false
@@ -89,7 +96,4 @@ puts boggle_board[2][3] == "x" #=> should be false
 # Reflection 
 # Should i know how to do this?  I can't find any good resources to explain arrays within arrays.
 
-# boggle_board = [["b", "r", "a", "e"],
-#                 ["i", "o", "d", "t"],
-#                 ["e", "c", "l", "r"],
-#                 ["t", "a", "k", "e"]]
+
